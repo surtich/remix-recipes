db664b98ecb2095aaac7e3e40adb73ad256b762a
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-26 19:05:55 +0200

    [loader] Se prueba la función loader en settings.tsx:
    - Obsérvese que al navegar a /settings se muestra el mensaje.
    - Si se abre la pestaña network se observa que se recibe toda la página de settings con el mensaje ya incluido porque se ha renderizado desde el servidor.
    - Si se navega a otro componente y se vuelve a /settings ahora el servidor sólo envía el mensaje.
    - El mensaje siempre es un JSON serilizado. Remox se encarga de forma automática de esto.
    - La función loader sólo se ejecuta en el servidor así que es seguro usar aquí accesos a base de datos, tokens, ...
    - Remix usa un objeto Response de la Fetch API estándar.

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = () => {
+  return { message: "Hello from the loader!" };
+};
 
 export default function SettingsLayout() {
+  const data = useLoaderData();
   return (
     <div>
       <h1>Settings Layout</h1>
+      <p>{data.message}</p>
       <Outlet />
     </div>
   );
