693fbbdffe1d11ee1e287352ec877bee53a91cc8
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-26 02:09:13 +0200

    [routes] Se mueve la función meta de index.tsx a root.tsx para que en todas las páginas se muestre el título. Esto se consigue gracias a la etiqueta <Meta /> de root.tsx.

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
