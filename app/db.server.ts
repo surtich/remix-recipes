5dfedfccd468641e40533c892fbe6a4dbf3078b6
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-27 01:10:48 +0200

    [pantry loader] Todavía hay un problema con la conexión de la base de datos. Aunque ahora se comparte entre loaders, cada vez que se recarga la aplicación (F5) se ejecuta el fichero y se crea una nueva conexión. Para evitarlo se crea un objeto global db y se devuelve si existe en vez de crear uno nuevo. Para evitar errores de TypeScript ha habido que extender la inferfaz Global del servidor (NodeJS).

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+interface CustomNodeJSGlobal extends NodeJS.Global {
+  db: PrismaClient;
+}
+
+declare const global: CustomNodeJSGlobal;
+
+const db = global.db || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.db = db;
 
 export default db;
