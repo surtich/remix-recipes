9740a794db3fec452825142dbaad741343d62ff4
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-27 01:00:44 +0200

    [pantry loader] Se recuperan las estanterías (shelves) de la base de datos."

--- app/routes/app.pantry.tsx
+++ app/routes/app.pantry.tsx
@@ -1,7 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+import { json, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  const db = new PrismaClient();
+  const shelves = await db.pantryShelf.findMany();
+  return json(shelves);
+};
+
 export default function Pantry() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div>
       <h1>Welcome to the pantry :)</h1>
+      <ul>
+        {data.map((shelf) => (
+          <li key={shelf.id}>{shelf.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
