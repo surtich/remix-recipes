21e58be3ce53b1a610280f945b2b6322d67a8e68
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-26 23:41:21 +0200

    [/app/pantry] Fix: El mt-2 del <nav> de app.tsx colapsa con el my-4 del <h1>. Este es el comportamiento por defecto de algunos componenentes de css. Se añade flex al contenedor para evitarlo.

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "@remix-run/react";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <h1 className="text-2xl font-bold my-4">App</h1>
       <nav className="border-b-2 pb-2 mt-2">
         <NavLink to="/pantry">Pantry</NavLink>
