833930552b4b6791f033dcb5976db30749487a63
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-26 23:34:19 +0200

    [/app/pantry] En app.tsx se crea un NavLink a /pantry (despensa).

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,8 +1,12 @@
+import { NavLink } from "@remix-run/react";
+
 export default function App() {
   return (
     <div>
       <h1 className="text-2xl font-bold my-4">App</h1>
-      <p>This is the app page.</p>
+      <nav className="border-b-2 pb-2 mt-2">
+        <NavLink to="/pantry">Pantry</NavLink>
+      </nav>
     </div>
   );
 }
