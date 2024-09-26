484b6d118181aefa6835bd8bc23ab32ae25348cb
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-26 23:31:30 +0200

    [/app] Se modifican los estilos de app.tsx.

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,7 +1,8 @@
 export default function App() {
   return (
     <div>
-      <h1>app</h1>
+      <h1 className="text-2xl font-bold my-4">App</h1>
+      <p>This is the app page.</p>
     </div>
   );
 }
