dfc144c7d77dd72a66663e619ed193fa73a1979f
Javier Pérez Arteaga <surtich@gmail.com>
2024-09-27 22:21:32 +0200

    [update shelf] Se muestra el mensaje de error si no se pasa la validación

--- app/routes/app.pantry.tsx
+++ app/routes/app.pantry.tsx
@@ -135,7 +135,7 @@ type ShelfProps = {
 
 function Shelf({ shelf }: ShelfProps) {
   const deleteShelfFetcher = useFetcher();
-  const saveShelfNameFetcher = useFetcher();
+  const saveShelfNameFetcher = useFetcher<typeof action>();
   const isDeletingShelf =
     deleteShelfFetcher.formData?.get("_action") === "deleteShelf" &&
     deleteShelfFetcher.formData?.get("shelfId") === shelf.id;
@@ -149,18 +149,26 @@ function Shelf({ shelf }: ShelfProps) {
         "md:w-96"
       )}
     >
-      <saveShelfNameFetcher.Form method="post" reloadDocument className="flex">
-        <input
-          type="text"
-          defaultValue={shelf.name}
-          name="shelfName"
-          placeholder="Shelf Name"
-          autoComplete="off"
-          className={classNames(
-            "text-2xl font-extrabold mb-2 w-full outline-none",
-            "border-b-2 border-b-background focus:border-b-primary"
-          )}
-        />
+      <saveShelfNameFetcher.Form method="post" className="flex">
+        <div className="w-full mb-2">
+          <input
+            type="text"
+            defaultValue={shelf.name}
+            name="shelfName"
+            placeholder="Shelf Name"
+            autoComplete="off"
+            className={classNames(
+              "text-2xl font-extrabold w-full outline-none",
+              "border-b-2 border-b-background focus:border-b-primary",
+              saveShelfNameFetcher.data?.errors?.shelfName
+                ? "border-b-red-600"
+                : ""
+            )}
+          />
+          <span className="text-red-600 text-sm">
+            {saveShelfNameFetcher.data?.errors?.shelfName}
+          </span>
+        </div>
         <button name="_action" value="saveShelfName" className="ml-4">
           <SaveIcon />
         </button>
