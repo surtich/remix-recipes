70e2a7718613301aa29e7f622caf1fe41267fbc1
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-01 21:48:09 +0200

    [login][sign up] Se crea un componente con export defaul de sign up. Ahora cuando se navega a la ruta /validate-magic-link se muestra ese componente a no ser que se haya introducido un usuario válido, en cuyo caso se redirige a /app.

--- app/routes/validate-magic-link.tsx
+++ app/routes/validate-magic-link.tsx
@@ -1,4 +1,6 @@
 import { json, redirect, type LoaderFunction } from "@remix-run/node";
+import classNames from "classnames";
+import { ErrorMessage, PrimaryButton, PrimaryInput } from "~/components/forms";
 import { getMagicLinkPayload, invalidMagicLink } from "~/magic-links.server";
 import { getUser } from "~/models/user.server";
 import { commitSession, getSession } from "~/sessions";
@@ -27,12 +29,50 @@ export const loader: LoaderFunction = async ({ request }) => {
   // 3. Validate email
   const user = await getUser(magicLinkPayload.email);
 
-  if (!user) {
-    throw invalidMagicLink("user not found");
+  if (user) {
+    session.set("userId", user.id);
+    return redirect("/app", {
+      headers: { "Set-Cookie": await commitSession(session) },
+    });
   }
 
-  session.set("userId", user.id);
-  return redirect("/app", {
+  return json("ok", {
     headers: { "Set-Cookie": await commitSession(session) },
   });
 };
+
+export default function ValidateMagicLink() {
+  return (
+    <div className="text-center">
+      <div className="mt-24">
+        <h1 className="text-2xl my8">You&apos;re almost done!</h1>
+        <h2>Type in your name bellow to complete the signup process.</h2>
+        <form
+          method="post"
+          className={classNames(
+            "flex flex-col px-8 mx-16 md:mx-auto",
+            "border-2 border-gray-200 rounded-md p-8 mt-8 md:w-80"
+          )}
+        >
+          <fieldset className="flex flex-col mb-8">
+            <div className="text-left mb-4">
+              <label htmlFor="firstName">First Name</label>
+              <PrimaryInput
+                id="firstName"
+                name="firstName"
+                autoComplete="off"
+              />
+              <ErrorMessage></ErrorMessage>
+            </div>
+            <div className="text-left">
+              <label htmlFor="lastName">Last Name</label>
+              <PrimaryInput id="lastName" name="lastName" autoComplete="off" />
+              <ErrorMessage></ErrorMessage>
+            </div>
+          </fieldset>
+          <PrimaryButton className="w-36 mx-auto">Sign Up</PrimaryButton>
+        </form>
+      </div>
+    </div>
+  );
+}
