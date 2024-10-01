14dc31a7b2adde992782a3bb7df01d2bf13765b7
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-01 18:02:28 +0200

    [login][cookie] Se añader una clave de firma de la cookie. Ahora cada vez que se cree la cookie se creará una firma (sinatura). Esto se puede ver si se va a la pestaña Application. La cookie almacenada tendrá dos partes separadas por un punto. La primera parte se corresponde con el valor y la segunda con la firma. Se se cambia manualmente el valor (en la pestaña Application) pretendiendo simular que se es otro usuario, en el servidor se puede ver que al refrescar la página se muestra que la cookie leída por el servidor es null. Aunque el cliente siga enviando la cookie, el método parse() comprueba que la signatura no es válida y devuelve null.

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,6 +1,13 @@
 import { createCookie } from "@remix-run/node";
 
+if (!process.env.AUTH_COOKIE_SECRET) {
+  throw new Error("AUTH_COOKIE_SECRET is not set");
+}
+
 export const userIdCookie = createCookie("remix-recipes__userId", {
+  // secrets es un array para que se pueda rotar la contraseña.
+  // Para firmar la cookie, se usa el primer secreto y para verificar la firma, se todos los del array.
+  secrets: [process.env.AUTH_COOKIE_SECRET],
   httpOnly: true,
   secure: true,
 });
