e8cbabf2f25154f4f66f2cf85703095632885fb4
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-01 18:10:39 +0200

    [login][session] Se renombra la cookie para que tenga un nombre estándar relacionado con sesiones.

--- app/cookies.ts
+++ app/cookies.ts
@@ -4,7 +4,7 @@ if (!process.env.AUTH_COOKIE_SECRET) {
   throw new Error("AUTH_COOKIE_SECRET is not set");
 }
 
-export const userIdCookie = createCookie("remix-recipes__userId", {
+export const sessionCookie = createCookie("remix-recipes__session", {
   // secrets es un array para que se pueda rotar la contraseña.
   // Para firmar la cookie, se usa el primer secreto y para verificar la firma, se todos los del array.
   secrets: [process.env.AUTH_COOKIE_SECRET],
