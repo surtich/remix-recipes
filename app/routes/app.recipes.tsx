34057cc2f031f982e4b1564616136f43972b413b
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-02 23:03:27 +0200

    [recipes][create] Se implementa la creación deuna receta. El redirect da error porque todavía no se ha creado la ruta /app/recipes/:id.

--- app/routes/app.recipes.tsx
+++ app/routes/app.recipes.tsx
@@ -1,6 +1,12 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
-import { SearchBar } from "~/components/forms";
+import {
+  type ActionFunctionArgs,
+  json,
+  type LoaderFunctionArgs,
+  redirect,
+} from "@remix-run/node";
+import { Form, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { PrimaryButton, SearchBar } from "~/components/forms";
+import { PlusIcon } from "~/components/icons";
 import {
   RecipeCard,
   RecipeDetailWrapper,
@@ -28,6 +34,21 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({ recipes });
 }
 
+export async function action({ request }: ActionFunctionArgs) {
+  const user = await requiredLoggedInUser(request);
+  const recipe = await db.recipe.create({
+    data: {
+      userId: user.id,
+      name: "New Recipe",
+      totalTime: "0 minutes",
+      imageUrl: "https://via.placeholder.com/150?text=Remix+Recipes",
+      instructions: "",
+    },
+  });
+
+  return redirect(`/app/recipes/${recipe.id}`);
+}
+
 export default function Recipes() {
   const data = useLoaderData<typeof loader>();
 
@@ -35,6 +56,14 @@ export default function Recipes() {
     <RecipePageWrapper>
       <RecipeListWrapper>
         <SearchBar placeholder="Search recipes..." />
+        <Form method="post" className="mt-4" reloadDocument>
+          <PrimaryButton className="w-full">
+            <div className="flex w-full justify-center">
+              <PlusIcon />
+              <span className="ml-2">Create New Recipe</span>
+            </div>
+          </PrimaryButton>
+        </Form>
         <ul>
           {data?.recipes.map((recipe) => (
             <li className="my-4" key={recipe.id}>
