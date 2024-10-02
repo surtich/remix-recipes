d9e35b25eb2bf8817217cf360d03ca1982287a65
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-02 22:32:50 +0200

    [recipes][recipes left list] Se muestra la lista de recetas de un usuario.

--- app/routes/app.recipes.tsx
+++ app/routes/app.recipes.tsx
@@ -1,3 +1,55 @@
+import { json, type LoaderFunctionArgs } from "@remix-run/node";
+import { NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import {
+  RecipeCard,
+  RecipeDetailWrapper,
+  RecipeListWrapper,
+  RecipePageWrapper,
+} from "~/components/recipes";
+import db from "~/db.server";
+import { requiredLoggedInUser } from "~/utils/auth.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await requiredLoggedInUser(request);
+
+  // En esta sección del código accedemos directamente a la base de datos para obtener las recetas del usuario
+  // En un proyecto real habría que decidir se se usan modelos o se accede directamente en la vista a la DB. Nunca se deberían mezclar ambas alternativas.
+  const recipes = await db.recipe.findMany({
+    where: {
+      userId: user.id,
+    },
+    select: { name: true, totalTime: true, imageUrl: true, id: true },
+  });
+
+  return json({ recipes });
+}
+
 export default function Recipes() {
-  return <div>Recipes</div>;
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <RecipePageWrapper>
+      <RecipeListWrapper>
+        <ul>
+          {data?.recipes.map((recipe) => (
+            <li className="my-4" key={recipe.id}>
+              <NavLink reloadDocument to={recipe.id}>
+                {({ isActive }) => (
+                  <RecipeCard
+                    name={recipe.name}
+                    totalTime={recipe.totalTime}
+                    imageUrl={recipe.imageUrl}
+                    isActive={isActive}
+                  />
+                )}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </RecipeListWrapper>
+      <RecipeDetailWrapper>
+        <Outlet />
+      </RecipeDetailWrapper>
+    </RecipePageWrapper>
+  );
 }
