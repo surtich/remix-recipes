23608a8ba1d89c3e767e33f1b4e1ae25e210ea80
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-07 20:34:12 +0200

    [meal plan][modal] Se soluciona el aviso y se asegura que sólo se ejecute el "hack" en cliente. Ahora aparece otro aviso.

--- app/routes/app.recipes.$recipeId.update-meal-plan.tsx
+++ app/routes/app.recipes.$recipeId.update-meal-plan.tsx
@@ -1,5 +1,9 @@
 import ReactModal from "react-modal";
 
+if (typeof window !== "undefined") {
+  ReactModal.setAppElement("body"); // un selector válido en css
+}
+
 export default function UpdateMealPlan() {
   return <ReactModal isOpen>Update Meal Plan</ReactModal>;
 }
