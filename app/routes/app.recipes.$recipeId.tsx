6dbe24132624ff6e4f6f4f62f3e67a85ab7c03b1
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-07 17:56:23 +0200

    [upload image] Se almacena la URL de la imagen en la base de datos.

--- app/routes/app.recipes.$recipeId.tsx
+++ app/routes/app.recipes.$recipeId.tsx
@@ -86,6 +86,7 @@ const saveIngredientNameSchema = z.object({
 
 const saveRecipeSchema = z
   .object({
+    imageUrl: z.string().optional(),
     ingredientIds: z.array(ingredientId).optional(),
     ingredientAmounts: z.array(ingredientAmount).optional(),
     ingredientNames: z.array(ingredientName).optional(),
@@ -154,6 +155,12 @@ export async function action({ request, params }: ActionFunctionArgs) {
       unstable_createMemoryUploadHandler()
     );
     formData = await unstable_parseMultipartFormData(request, uploadHandler);
+    const image = formData.get("image") as File;
+
+    if (image.size !== 0) {
+      const imageUrl = `/images/${image.name}`;
+      formData.set("imageUrl", imageUrl);
+    }
   } else {
     formData = await request.formData();
   }
