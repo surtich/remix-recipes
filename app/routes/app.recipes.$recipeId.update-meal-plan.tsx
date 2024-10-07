c5294de74e5dcec3783df58e9d1795c84ac5842f
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-07 22:20:55 +0200

    [meal plan][modal] Se implementa la acción que permita guardar y borrar la receta en la dieta. De momento no funciona ya que, aunque el formulario envía un tipo number, al serializar la información del formulario en la llamada de red, se convierte a string.

--- app/routes/app.recipes.$recipeId.update-meal-plan.tsx
+++ app/routes/app.recipes.$recipeId.update-meal-plan.tsx
@@ -1,14 +1,57 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useActionData } from "@remix-run/react";
 import ReactModal from "react-modal";
-import { DeleteButton, IconInput, PrimaryButton } from "~/components/forms";
+import {
+  DeleteButton,
+  ErrorMessage,
+  IconInput,
+  PrimaryButton,
+} from "~/components/forms";
 import { XIcon } from "~/components/icons";
 import { useRecipeContext } from "./app.recipes.$recipeId";
-import { ActionFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
 import { canChangeRecipe } from "~/utils/abilities.server";
+import db from "~/db.server";
+import { z } from "zod";
+import { validateForm } from "~/utils/validation";
+
+const updateMealPlanSchema = z.object({
+  mealPlanMultiplier: z.number().min(1),
+});
 
 export async function action({ request, params }: ActionFunctionArgs) {
   const recipeId = String(params.recipeId);
   canChangeRecipe(request, recipeId);
+
+  const formData = await request.formData();
+
+  switch (formData.get("_action")) {
+    case "updateMealPlan": {
+      return validateForm(
+        formData,
+        updateMealPlanSchema,
+        async ({ mealPlanMultiplier }) => {
+          await db.recipe.update({
+            where: { id: recipeId },
+            data: { mealPlanMultiplier },
+          });
+          return redirect("..");
+        },
+        (errors) => json({ errors }, { status: 400 })
+      );
+    }
+
+    case "removeFromMealPlan": {
+      await db.recipe.update({
+        where: { id: recipeId },
+        data: { mealPlanMultiplier: null },
+      });
+      return redirect("..");
+    }
+
+    default: {
+      return null;
+    }
+  }
 }
 
 if (typeof window !== "undefined") {
@@ -17,6 +60,7 @@ if (typeof window !== "undefined") {
 
 export default function UpdateMealPlan() {
   const { recipeName, mealPlanMultiplier } = useRecipeContext();
+  const actionData = useActionData<any>();
   return (
     <ReactModal isOpen className="md:h-fit lg:w-1/2 md:mx-auto md:mt-24">
       <div className="p-4 rounded-md bg-white shadow-md">
@@ -36,8 +80,9 @@ export default function UpdateMealPlan() {
             defaultValue={mealPlanMultiplier ?? 1}
             type="number"
             autoComplete="off"
-            name="mealPlanMuiltiplier"
+            name="mealPlanMultiplier"
           />
+          <ErrorMessage>{actionData?.errors?.mealPlanMultiplier}</ErrorMessage>
           <div className="flex justify-end gap-4 mt-8">
             {mealPlanMultiplier !== null ? (
               <DeleteButton name="_action" value="removeFromMealPlan">
