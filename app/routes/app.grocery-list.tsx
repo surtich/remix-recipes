d321056b4a73879f7dfa201d372d85143ffac710
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-08 08:30:22 +0200

    [grocery-list] Se implementa el loader.

--- app/routes/app.grocery-list.tsx
+++ app/routes/app.grocery-list.tsx
@@ -1,7 +1,119 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { CheckCircleIcon } from "~/components/icons";
+import db from "~/db.server";
+import { requiredLoggedInUser } from "~/utils/auth.server";
+
+type GroceryListItem = {
+  id: string;
+  name: string;
+  uses: Array<{
+    id: string;
+    amount: string | null;
+    recipeName: string;
+    multiplier: number;
+  }>;
+};
+
+function isMatch(ingredientName: string, pantryItemName: string) {
+  // TODO: habría que mejorar esto usando expresiones regulares.
+  return ingredientName.toLowerCase() === pantryItemName.toLowerCase();
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await requiredLoggedInUser(request);
+
+  const ingredients = await db.ingredient.findMany({
+    where: {
+      recipe: {
+        userId: user.id,
+        mealPlanMultiplier: { not: null },
+      },
+    },
+    include: {
+      recipe: {
+        select: {
+          name: true,
+          mealPlanMultiplier: true,
+        },
+      },
+    },
+  });
+
+  const pantryItems = await db.pantryItem.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  const missingIngredients = ingredients.filter(
+    (ingredient) =>
+      !pantryItems.find((pantryItem) =>
+        isMatch(ingredient.name, pantryItem.name)
+      )
+  );
+
+  const groceryListItems = missingIngredients.reduce<{
+    [key: string]: GroceryListItem;
+  }>((groceryListItemsMapSoFar, ingredient) => {
+    const ingredientName = ingredient.name.toLowerCase(); // TODO: mejorar con expresiones regulares
+    const existing = groceryListItemsMapSoFar[ingredientName] ?? { uses: [] };
+
+    return {
+      ...groceryListItemsMapSoFar,
+      [ingredientName]: {
+        id: ingredient.id,
+        name: ingredientName,
+        uses: [
+          ...existing.uses,
+          {
+            id: ingredient.recipeId,
+            amount: ingredient.amount,
+            recipeName: ingredient.recipe.name,
+            multiplier: ingredient.recipe.mealPlanMultiplier!, // estamos seguro de que la receta está en el mealPlanMultiplier,
+          },
+        ],
+      },
+    };
+  }, {});
+
+  return { groceryListItems: Object.values(groceryListItems) };
+}
+
+function GroceryListItem({ item }: { item: GroceryListItem }) {
+  const fetcher = useFetcher();
+  return fetcher.state !== "idle" ? null : (
+    <div className="shadow-md rounded-md p-4 flex">
+      <div className="flex-grow">
+        <h1 className="text-sm font-bold mb-2 uppercase">{item.name}</h1>
+        <ul>
+          {item.uses.map((use) => (
+            <li key={use.id} className="py-1">
+              {use.amount} for {use.recipeName} (x{use.multiplier})
+            </li>
+          ))}
+        </ul>
+      </div>
+      <fetcher.Form method="post" className="flex flex-col justify-center">
+        <button
+          name="_action"
+          value="checkOffItem"
+          className="hover:text-primary"
+        >
+          <CheckCircleIcon />
+        </button>
+      </fetcher.Form>
+    </div>
+  );
+}
+
 export default function GroceryList() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <div>
-      <h1>Grocery List</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      {data.groceryListItems.map((item) => (
+        <GroceryListItem key={item.id} item={item} />
+      ))}
     </div>
   );
 }
