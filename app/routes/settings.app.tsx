8e769ba7abbbc2dfcc3f561c7f97ef70a0b43df6
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-08 19:19:02 +0200

    [settings] Se crea un loader para que la lista desplegable muestre el color almacenado en la cookie. Se comprueba que hay un problema: * Si se selecciona un color y se pulsa save, se comprueba que la lista selecciona el color anteriormente elegido. La lista muestra el valor anterior de la cookie. * Sin embargo, si se recarga (F5) la página, la lista muestra el color correcto.
    ¿Por qué ocurre esto?
    
    Si se selecciona un color y se pulsa save, en la pestaña Network se observa que en el Response Headers hay una cookie con el color elegido, pero
    en el Request Headers la cookie tiene el valor anterior. Lo que estamos leyendo es la cookie que ahora tiene el navegador (que es la misma que el Request Headers).
    
    Una forma fácil de solucionarlo es eliminar "reloadDocument" pero esto obliga a que el cliente tenga que tener activado JavaScript ya que la página no se recargará.
    Funciona porque Remix enviará la peticion a la acción primero y luego al loader. Esta es la revalidación de remix: todos los loaders de la ruta se vuelven a ejecutar después d ehaber completado la acción.
    
    Es este caso no tiene sentido esta solución incluso si el cliente tiene JavaScript activado ya que necesitamos recargar toda la página para el cambio de colores tenga efecto.

--- app/routes/settings.app.tsx
+++ app/routes/settings.app.tsx
@@ -1,9 +1,16 @@
-import { ActionFunctionArgs, json } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { z } from "zod";
 import { PrimaryButton } from "~/components/forms";
 import { themeCookie } from "~/cookies";
 import { validateForm } from "~/utils/validation";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const cookieHeader = request.headers.get("Cookie");
+  const theme = await themeCookie.parse(cookieHeader);
+
+  return { theme: typeof theme === "string" ? theme : "green" };
+}
 
 const themeSchema = z.object({
   theme: z.string(),
@@ -23,16 +30,17 @@ export async function action({ request }: ActionFunctionArgs) {
 }
 
 export default function SettingApp() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <Form reloadDocument method="post">
       <div className="mb-4 flex flex-col">
-        <label htmlFor="theme" defaultValue="green">
-          Theme
-        </label>
+        <label htmlFor="theme">Theme</label>
         <select
           id="theme"
           name="theme"
           className="p-2 mt-2 border-2 border-gray-200 rounded-md w-full md:w-64"
+          defaultValue={data.theme}
         >
           <option value="red">Red</option>
           <option value="orange">Orange</option>
