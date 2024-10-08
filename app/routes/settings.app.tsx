ef21ef28b83ec6ca22d030c955308416d41402b9
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-08 18:47:24 +0200

    [settings] Se crea una lista desplegable en la ruta /settings/app con los colores que puede elegir el usuario.

--- app/routes/settings.app.tsx
+++ app/routes/settings.app.tsx
@@ -1,7 +1,27 @@
+import { Form } from "@remix-run/react";
+import { PrimaryButton } from "~/components/forms";
+
 export default function SettingApp() {
   return (
-    <div>
-      <h1>setting app</h1>
-    </div>
+    <Form reloadDocument method="post">
+      <div className="mb-4 flex flex-col">
+        <label htmlFor="theme" defaultValue="green">
+          Theme
+        </label>
+        <select
+          id="theme"
+          name="theme"
+          className="p-2 mt-2 border-2 border-gray-200 rounded-md w-full md:w-64"
+        >
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="purple">Purple</option>
+        </select>
+      </div>
+      <PrimaryButton>Save</PrimaryButton>
+    </Form>
   );
 }
