3481f464cf78644830af47dd00dea7db920be761
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-09 16:32:45 +0200

    [playwright] Se protegen estas rutas de test mediante una variable de entorno que debe ser pasada durante los tests.

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,19 @@ export default defineConfig({
         v3_throwAbortReason: true,
       },
       routes: (defineRoutes) =>
+        //ver: https://remix.run/docs/en/main/file-conventions/vite-config#routes
         defineRoutes((route) => {
-          //ver: https://remix.run/docs/en/main/file-conventions/vite-config#routes
-          route("__tests/login", "__test-routes__/login.tsx");
+          // Sólo se incluye esta ruta si existe esta variable de entorno
+          if (process.env.INCLUDE_TEST_ROUTES) {
+            // Protegemos a los desarrolladores de que hagan cosas estúpidas como incluir rutas de prueba en producción
+            if (process.env.NODE_ENV === "production") {
+              console.warn(
+                "You should not include test routes in production."
+              );
+              return;
+            }
+            route("__tests/login", "__test-routes__/login.tsx");
+          }
         }),
     }),
     tsconfigPaths(),
