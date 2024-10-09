0db943444a60fd2edba3fe9fa1cb3887f4a1d4b6
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-09 16:46:30 +0200

    [playwright] Se implementa la ruta /__tests/login Esta ruta recibe los parámetros que permiten comprobar si un usuario existe o crearlo si no fuera así. Crea la cookie de sesión y redirige a /app
    Para probarla hay que arrancar el servidor así:
    INCLUDE_TEST_ROUTES=true npm run dev
    
    Y luego navegar a la ruta pasándo los parámetros. Por ejemplo:
    http://localhost:3000/__tests/login?email=test@remix.com&firstName=Test&lastName=Remix

--- app/__test-routes__/login.tsx
+++ app/__test-routes__/login.tsx
@@ -1,3 +1,34 @@
-export async function loader() {
-  return "You're at the test login route!";
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { createUser, getUser } from "~/models/user.server";
+import { commitSession, getSession } from "~/sessions";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const firstName = url.searchParams.get("firstName");
+  const lastName = url.searchParams.get("lastName");
+  const email = url.searchParams.get("email");
+
+  if (!email) {
+    throw new Error("Email is required");
+  }
+
+  let user = await getUser(email);
+
+  if (!user) {
+    if (!firstName || !lastName) {
+      throw new Error("First name and last name are required");
+    }
+
+    user = await createUser(email, firstName, lastName);
+  }
+
+  const cookie = request.headers.get("cookie");
+  const session = await getSession(cookie);
+  session.set("userId", user.id);
+
+  return redirect("/app", {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
 }
